df6f77a40106b9f52a8d99beeaeae7088c277fda
IreneJebet <ireenjebetu@gmail.com>
2023-05-11 02:49:34 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*
+ * file : main.h
+ * Desc: header file containing prototypes for all functions
+         written in the 0x15-file_io directory.
+ */
 
 #include <sys/types.h>
 #include <sys/types.h>
@@ -13,6 +18,6 @@ int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *src, const char *dest);
-int _putchar(char c)
+int _putchar(char c);
 
 #endif
